15c27c34cf88486612dbbd24102aed65254c9979
Manual do Front <contatomanualdofront@gmail.com>
2025-11-18 20:42:04 -0300

    chore(v3): create second test - TaskItem

--- src/components/TaskItem.spec.tsx
+++ src/components/TaskItem.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { TaskItem } from './TaskItem';
 
 describe('TaskItem', () => {
@@ -24,4 +25,25 @@ describe('TaskItem', () => {
     const title = screen.getByText('Assistir a playlist de testes do Manual do Front'); // ou mocTask.title
     expect(title).toBeInTheDocument();
   });
+
+  test('chama onToggle quando checkbox é clicado', async () => {
+    const user = userEvent.setup();
+    
+    render(
+      <TaskItem 
+        task={mockTask}
+        onDelete={mockOnDelete}
+        onToggle={mockOnToggle}
+      />
+    );
+
+    const checkbox = screen.getByRole('checkbox', { 
+      name: mockTask.title
+    });
+    
+    await user.click(checkbox);
+
+    expect(mockOnToggle).toHaveBeenCalledTimes(1);
+    expect(mockOnToggle).toHaveBeenCalledWith("1"); // ID da task
+  }); 
 });
